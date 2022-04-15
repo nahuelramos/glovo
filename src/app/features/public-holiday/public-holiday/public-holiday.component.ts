cf7044bdc20cc4b90adc7b4054f843fed7ccd41c
Nahuel Ramos <UV2YXHV>
2022-04-15 11:36:02 +0200

    master - create new feature public holiday & add prettier commands

---
+++ src/app/features/public-holiday/public-holiday/public-holiday.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-holiday',
+  templateUrl: './public-holiday.component.html',
+  styleUrls: ['./public-holiday.component.scss'],
+})
+export class PublicHolidayComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/public-holiday/public-holiday/public-holiday.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicHolidayComponent } from './public-holiday.component';
+
+@NgModule({
+  declarations: [PublicHolidayComponent],
+  imports: [CommonModule],
+})
+export class PublicHolidayModule {}
